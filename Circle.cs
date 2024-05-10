c16dad3b340c320e504f28b85299b5111eaef901
Victoria Klaus <vlklaus22@gmail.com>
2024-05-10 16:53:00 -0400

    first commit minus extra challenge

---
+++ Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _051024_Circle_Objects_Lab
+{
+    public class Circle
+    {
+        // properties
+        private double Radius { get; set; }
+
+
+        // constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+
+        // method
+        public double CalculateDiameter()
+        {
+           double diameter = Radius * 2;
+           return diameter;
+        }
+
+        public double CalculateCircumference()
+        {
+            double circumference = 2 * System.Math.PI * Radius;
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = System.Math.PI * Math.Pow(Radius, 2);
+            return area;
+        }
+
+        public void Grow()
+        {
+            Radius *= 2;
+        }
+
+        public double GetRadius()
+        {
+            double radius = (2 * System.Math.PI * Radius) / (2 * System.Math.PI);
+            return radius;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using _051024_Circle_Objects_Lab;
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+
+Console.Write("Enter radius: ");
+double userRadius;
+while (double.TryParse(Console.ReadLine(), out userRadius) == false || userRadius < 0.1)
+{
+    Console.WriteLine("Please enter a valid number.");
+}
+
+Circle value = new Circle(userRadius);
+
+Console.WriteLine($"Diameter: {value.CalculateDiameter()}");
+Console.WriteLine($"Circumference: {value.CalculateCircumference()}");
+Console.WriteLine($"Area: {value.CalculateArea()}");
+
+
+bool increaseCircle = true;
+while (increaseCircle)
+{
+    Console.WriteLine("Should the circle grow? y/n");
+    string growCircle = Console.ReadLine();
+
+    if (growCircle == "y")
+    {
+        value.Grow();
+
+        Console.WriteLine($"Diameter: {value.CalculateDiameter()}");
+        Console.WriteLine($"Circumference: {value.CalculateCircumference()}");
+        Console.WriteLine($"Area: {value.CalculateArea()}");
+        break;
+    } else if (growCircle == "n")
+    {
+        increaseCircle = false;
+        break;
+    } else
+    {
+        Console.WriteLine("Invalid response.");
+    }
+}
+
+Console.WriteLine($"Goodbye. The circle's final radius is {value.GetRadius()}");
